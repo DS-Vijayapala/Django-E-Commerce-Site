0d33a229b0887a27aafe6f75e141dbec30b91a73
Dineth <dinethfiverr23@gmail.com>
2025-03-20 12:14:21 +0530

    Adding Products To Database And Adding Catagory Options

--- Shop/models.py
+++ Shop/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+options = (
+    ('fashion', 'Fashion'),
+    ('electronics', 'Electronics'),
+    ('sports', 'Sports'),
+    ('Gaming', 'Gaming'),
+)
+
 class Product(models.Model):
 
     title = models.CharField(max_length=200)
     price = models.FloatField()
     discount_price = models.FloatField(blank=True, null=True)
-    catogaory = models.CharField(max_length=200)
+    catogaory = models.CharField(max_length=200 , choices=options)
     description = models.TextField()
     image = models.CharField(max_length=3000)
 
