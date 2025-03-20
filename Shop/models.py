9a43f4c4e4aefeddcc3d2d81d8c6fa7c1098d21c
Dineth <dinethfiverr23@gmail.com>
2025-03-20 11:25:09 +0530

    Creating Product Model

--- Shop/models.py
+++ Shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    catogaory = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=3000)
+
+    def __str__(self):
+        return self.title
