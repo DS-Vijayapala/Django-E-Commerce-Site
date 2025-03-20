9507a1ca902c2a68e67f7bb95a89ac5a1ec37983
Dineth <dinethfiverr23@gmail.com>
2025-03-20 12:31:41 +0530

    Building The Index View

--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def index(request):
+    """A view that displays the index page"""
+
+    products_object = Product.objects.all()
+
+    context = {
+        'products_object': products_object
+    }
+
+    return render(request, 'shop\index.html', context)
