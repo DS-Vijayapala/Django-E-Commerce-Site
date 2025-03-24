8721f1d6b98797d28f8b21f57ece6ba416a2fc3d
Dineth <dinethfiverr23@gmail.com>
2025-03-24 14:48:59 +0530

    Updating Header , Title , Index Title For Customisation Admin Panal

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Product, Order
 
-# Register your models here.
+
+admin.site.site_header = 'Avix Tecno Shop Admin Area'
+admin.site.site_title = 'Avix Tecno Shop'
+admin.site.index_title = 'Welcome to the Avix , '
 
 admin.site.register(Product)
 admin.site.register(Order)
